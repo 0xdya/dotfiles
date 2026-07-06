72f16271150fd339069652add26701f07284d766
0xdya <0xdya@proton.me>
2026-07-06 20:46:53 +0100

    Add files via upload

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,13 @@
-# fastfetch
-# Created by `pipx` on 2025-11-04 10:53:28
-set PATH $PATH /home/dya/.local/bin
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+abbr -a b 'ddcutil setvcp 10'
+abbr -a u 'sudo pacman -Syu'
+abbr -a i 'sudo pacman -Sy'
+abbr -a iy 'yay -Sy'
+abbr -a d 'aria2c -x 16 -s 16'       # عشان تحمل بسرعة كبيرة
+abbr -a s 'snapper -c root create -d "Snapshot"'
+abbr -a sl 'snapper -c root list'
+abbr -a f 'fastfetch'
+abbr -a mc 'cd ~/mc-server && java -Xmx4G -Xms2G -jar fabric-server-mc.1.21.11-loader.0.19.3-launcher.1.1.1.jar nogui'
